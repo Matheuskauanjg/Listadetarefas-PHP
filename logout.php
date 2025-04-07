dbecc3c5394fcd8164fa3b31c57459a35291e86e
Matheus kauan pinto <mkauanpinto@gmail.com>
2025-04-06 21:08:19 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
 
-// Destrói todas as variáveis de sessão
 $_SESSION = array();
 
-// Destrói a sessão
 session_destroy();
 
-// Redireciona para login com mensagem de sucesso
 $_SESSION['mensagem'] = "Logout realizado com sucesso!";
 header('Location: login.php');
 exit();
@@ -19,7 +16,6 @@ exit();
     <meta http-equiv="refresh" content="0;url=login.php">
 </head>
 <body>
-    <!-- Feedback visual temporário caso o redirecionamento falhe -->
     <div class="container mt-5">
         <div class="alert alert-info">
             Logout realizado com sucesso. Redirecionando...
